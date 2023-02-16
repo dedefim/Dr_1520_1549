1cd696fc5437267d2c25bfe9e1474afc28f18487
efim <dedefim56@gmail.com>
2023-02-16 23:04:42 +0500

    lesson4

--- library/library/users/views.py
+++ library/library/users/views.py
@@ -1,8 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserModelSerializer
-
+from rest_framework import serializers
+from rest_framework import mixins
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+class UsersSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = users
+        fields = '__all__'
+
+class UserLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 100
+class USerLimitOffsetPaginatonViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class =UserSerializer
+    pagination_class = UserleLimitOffsetPagination
+
+class UserCustomViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
--- library/library/write/views.py
+++ library/library/write/views.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from .models import TODO, Project
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import viewsets
+from rest_framework import mixins
+
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
@@ -11,4 +15,27 @@ class TODOSerializer(serializers.HyperlinkedModelSerializer):
 
 
 
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 100
+class TODOLimitOffsetPaginatonViewSet(viewsets.ModelViewSet):
+    queryset = TODO.objects.all()
+    serializer_class =TODOSerializer
+    pagination_class = TODOleLimitOffsetPagination
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 100
+class ProjectLimitOffsetPaginatonViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class =ProjectSerializer
+    pagination_class = ProjectleLimitOffsetPagination
 
+
+class ProjectCustomViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    renderer_classes = [JSORenderer, BrowsableAPIRenderer]
+
+class TODOCustomViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+    mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = TODO.objects.all()
+    serializer_class = TODOleSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
