c4606e6c3a4b404e2951a0a371462f2c5f4e38a1
efim <dedefim56@gmail.com>
2023-03-08 15:13:44 +0500

    lesson9

--- library/library/library/urls.py
+++ library/library/library/urls.py
@@ -22,6 +22,25 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
+router = DefaultRouter()
+router.register('Users', UserModelViewSet)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+name='schema-redoc'),
+
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+]
 
 schema_view = get_schema_view(
     openapi.Info(
